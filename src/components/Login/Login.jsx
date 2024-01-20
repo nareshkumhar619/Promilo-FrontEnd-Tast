63284524801c6e1cbfe0a038d6904639c75fbf76
Naresh Kumhar <122338149+nareshkumhar619@users.noreply.github.com>
2024-01-21 01:51:01 +0530

    commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import validation from '../../validationRules.json'
 import { auth, provider } from '../../GoogleAuth/config'
 import { signInWithPopup } from 'firebase/auth'
-import image from '../../images/loginbg.jpg'
+import image from '../../images/images.jpeg'
 import store from '../../images/prov-store.png'
 import google from '../../images/google.png'
 import right from '../../images/check.png';
